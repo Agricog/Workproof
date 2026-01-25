413bdb83567a91343d12c60f1e823df6120ed779
Agricog <mick@mickstevenson.com>
2026-01-25 15:36:49 +0000

    Create index.ts

---
+++ src/types/index.ts
@@ -0,0 +1,16 @@
+/**
+ * WorkProof Types Index
+ * Central export point for all type definitions
+ */
+
+// Core data models
+export * from './models'
+
+// Task configurations and evidence types
+export * from './taskConfigs'
+
+// API types, sync, and offline support
+export * from './api'
+
+// Security types
+export * from './security'
