627551af65f5aca70c923e7b759c15ae53d095b4
Agricog <mick@mickstevenson.com>
2026-01-25 15:56:57 +0000

    Create index.ts

---
+++ src/utils/index.ts
@@ -0,0 +1,22 @@
+/**
+ * WorkProof Utils Index
+ * Central export for all utilities
+ */
+
+// Validation
+export * from './validation'
+
+// Cryptography
+export * from './crypto'
+
+// Image compression
+export * from './compression'
+
+// Error tracking
+export * from './errorTracking'
+
+// IndexedDB storage
+export * from './indexedDB'
+
+// CSRF protection
+export * from './csrf'
