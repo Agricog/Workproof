0129071f57932a3dcd0d6f08d760f4f110077b03
Agricog <mick@mickstevenson.com>
2026-01-27 17:15:55 +0000

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -35,7 +35,6 @@ export default function Layout({ children }: LayoutProps) {
 
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
-      {/* Skip Navigation - WCAG 2.4.1 */}
       
         href="#main-content"
         className="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-50 focus:px-4 focus:py-2 focus:bg-blue-600 focus:text-white focus:rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
@@ -43,15 +42,12 @@ export default function Layout({ children }: LayoutProps) {
         Skip to main content
       </a>
 
-      {/* Main content */}
       <main id="main-content" className="flex-1 pb-20" tabIndex={-1}>
         <div className="max-w-lg mx-auto px-4 py-6">{children}</div>
       </main>
 
-      {/* Bottom navigation */}
       <Navigation />
 
-      {/* Safe area for iOS */}
       <div className="safe-area-bottom" />
     </div>
   )
