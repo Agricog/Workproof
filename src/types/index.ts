7eecbb90cf3303faa754831d4fc264b121c31dae
Agricog <mick@mickstevenson.com>
2026-01-29 10:59:07 +0000

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,16 +2,12 @@
  * WorkProof Types Index
  * Central export point for all type definitions
  */
+
 // Core data models
 export * from './models'
 
-// Task configurations and evidence types (exclude duplicate)
-export { 
-  TASK_CONFIGS,
-  getTaskTypeConfig,
-  getEvidenceLabel,
-  type TaskTypeConfig 
-} from './taskConfigs'
+// Task configurations and evidence types
+export * from './taskConfigs'
 
 // API types, sync, and offline support
 export * from './api'
