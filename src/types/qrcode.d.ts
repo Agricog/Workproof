4e1bf461b33e58568914efcd2392c671fd5c772e
Agricog <mick@mickstevenson.com>
2026-02-01 14:00:28 +0000

    Create qrcode.d.ts

---
+++ src/types/qrcode.d.ts
@@ -0,0 +1,14 @@
+declare module 'qrcode' {
+  interface QRCodeOptions {
+    width?: number
+    margin?: number
+    color?: {
+      dark?: string
+      light?: string
+    }
+  }
+  
+  export function toDataURL(text: string, options?: QRCodeOptions): Promise<string>
+  export function toString(text: string, options?: QRCodeOptions): Promise<string>
+  export function toBuffer(text: string, options?: QRCodeOptions): Promise<Buffer>
+}
