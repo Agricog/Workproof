8093c212dc9b3002af5b5604ed423a9cdf61e894
Agricog <mick@mickstevenson.com>
2026-02-01 20:57:36 +0000

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,6 @@ import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 import { logger } from 'hono/logger'
 import { secureHeaders } from 'hono/secure-headers'
-
 // Routes
 import users from './routes/users.js'
 import jobs from './routes/jobs.js'
@@ -14,7 +13,6 @@ import imageProxy from './routes/imageProxy.js'
 import verify from './routes/verify.js'
 import admin from './routes/admin.js'
 import transcribe from './routes/transcribe.js'
-
 // Webhooks
 import clerkWebhook from './webhooks/clerk.js'
 
@@ -22,7 +20,13 @@ const app = new Hono()
 
 // Middleware
 app.use('*', logger())
-app.use('*', secureHeaders())
+app.use('*', secureHeaders({
+  permissionsPolicy: {
+    camera: ['self'],
+    microphone: ['self'],
+    geolocation: ['self'],
+  }
+}))
 app.use('*', cors({
   origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true,
